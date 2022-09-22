53141ea556fc01ecad44f58b30d02386b6b5a65b
Vikram Chaurasia <chaurasiavikram23@gmail.com>
2022-09-23 02:12:12 +0530

    pushing

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,3 +218,7 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+
+
+
